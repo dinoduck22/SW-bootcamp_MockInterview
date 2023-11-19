115a65dfa5a80fa3874243ebbcb938909c9e8857
dinoduck22 <ha6948@cu.ac.kr>
2023-11-19 23:01:31 +0900

    setting : config.toml AI Image : jobinterview.gif, etc main.py : added record, sidebar chat-log, AI-Image

---
+++ ReadMe.md
@@ -0,0 +1,11 @@
+# Recommnad
+Virtual Environment (ex. anaconda)
+
+# Dependancies
+* <code> Python >= 3.8 && != 3.9 </code>
+* <code> Streamlit >= 1.24.1 </code>
+
+# installation
+* <code> pip install streamlit --upgrade </code>
+* <code> pip install steramlit-chat </code> #stream_chat
+* <code> pip install opencv-python streamlit Pillow </code> #open cv 
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import cv2
 import streamlit as st
-import numpy as np
 from PIL import Image
+import base64
+from st_audiorec import st_audiorec
 
 # 레이아웃
 st.set_page_config(layout="wide")
@@ -10,6 +10,8 @@ if 'count' not in st.session_state:  # 버튼 클릭 횟수
     st.session_state.count = 0
 if 'caption' not in st.session_state:  # 버튼 내용
     st.session_state['caption'] = 'caption place'
+if 'message' not in st.session_state:  # chat history
+    st.session_state.message = []
 
 
 def caption(button):
@@ -21,26 +23,55 @@ def caption(button):
 
 
 def record():
-    st.text_input()
+    # recodring
+    wav_audio_data = st_audiorec()
+    if wav_audio_data is not None:
+        st.audio(wav_audio_data, format='audio/wav')
 
 
 # 사이드 바
 def sidebar():
-    st.sidebar.title("Chat log")
+    st.sidebar.title("Chat Log")
+    for message in st.session_state.message:  # Display chat history
+        with st.sidebar.chat_message(message["role"]):
+            st.markdown(message["content"])
+    # Chat Input
+    if prompt := st.chat_input("Type in your Answer"):
+        with st.sidebar.chat_message("user"):
+            st.markdown(prompt)  # Display user message
+        st.session_state.message.append({"role": "user", "content": prompt})  # Add to chat history
+    # AI response
+    question = "Hello, this is AI mock Interview."
+    with st.sidebar.chat_message("assistant"):
+        st.markdown(question)  # Display assistant response
+    st.session_state.message.append({"role": "assistant", "content": question})  # Add to chat history
 
 
 def main():
+    # 사이드 바
     sidebar()
+    # 내용
     st.title("AI mock interview")
-    st.text("We use OpenCV and Streamlit for this demo")
-    image = Image.open('tony_stark.jpg')
-    st.image(image, use_column_width=True)
-    col1, col2 = st.columns([1, 1])  # 비율 col1:col2:col3
+    st.text("To start the mock Interview, answer the following question")
+    # AI 비디오
+    file_ = open("jobinterview.gif", "rb")
+    contents = file_.read()
+    data_url = base64.b64encode(contents).decode("utf-8")
+    file_.close()
+    st.markdown(
+        f'<img src="data:image/gif;base64,{data_url}" alt="job-interview" width="100%">',
+        unsafe_allow_html=True,
+    )
+    # 버튼
+    col1, col2 = st.columns([1, 1])  # 비율 col1:col2
     with col1:
+        st.text('')  # 줄바꿈
         st.button(label="caption", on_click=caption('clicked'))
     with col2:
+        st.text('')  # 줄바꿈
         st.text(st.session_state['caption'])
-        st.button(label="record", on_click=' ')
+    # Recording
+    record()
 
 
 if __name__ == '__main__':
