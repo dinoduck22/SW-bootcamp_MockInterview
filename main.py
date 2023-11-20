c53ee3f563a8afa39b972aaabf07755e7d4b955a
dinoduck22 <ha6948@cu.ac.kr>
2023-11-21 00:18:37 +0900

    fixed chat log history duplication, screen ratio, screen middle sort

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from PIL import Image
 import base64
 from st_audiorec import st_audiorec
 
@@ -10,15 +9,17 @@ if 'count' not in st.session_state:  # 버튼 클릭 횟수
     st.session_state.count = 0
 if 'caption' not in st.session_state:  # 버튼 내용
     st.session_state['caption'] = 'caption place'
+# AI response
+question = "Hello, this is AI mock Interview."
 if 'message' not in st.session_state:  # chat history
-    st.session_state.message = []
+    st.session_state.message = [{"role": "assistant", "content": question}]
 
 
-def caption(button):
-    st.session_state['caption'] = button
+def caption(string):
+    st.session_state['caption'] = string
     st.session_state.count += 1
     if st.session_state.count == 2:
-        st.session_state['caption'] = 'caption'  # 버튼 클릭 시 표기
+        st.session_state['caption'] = 'caption place'  # 버튼 클릭 시 표기
         st.session_state.count = 0
 
 
@@ -32,19 +33,18 @@ def record():
 # 사이드 바
 def sidebar():
     st.sidebar.title("Chat Log")
-    for message in st.session_state.message:  # Display chat history
+    # Display chat history
+    for message in st.session_state.message:
         with st.sidebar.chat_message(message["role"]):
             st.markdown(message["content"])
-    # Chat Input
+    # Chat Input & AI question
     if prompt := st.chat_input("Type in your Answer"):
         with st.sidebar.chat_message("user"):
             st.markdown(prompt)  # Display user message
         st.session_state.message.append({"role": "user", "content": prompt})  # Add to chat history
-    # AI response
-    question = "Hello, this is AI mock Interview."
-    with st.sidebar.chat_message("assistant"):
-        st.markdown(question)  # Display assistant response
-    st.session_state.message.append({"role": "assistant", "content": question})  # Add to chat history
+        with st.sidebar.chat_message("assistant"):
+            st.markdown(question)  # Display assistant response
+        st.session_state.message.append({"role": "assistant", "content": question})  # Add to chat history
 
 
 def main():
@@ -58,20 +58,25 @@ def main():
     contents = file_.read()
     data_url = base64.b64encode(contents).decode("utf-8")
     file_.close()
-    st.markdown(
-        f'<img src="data:image/gif;base64,{data_url}" alt="job-interview" width="100%">',
-        unsafe_allow_html=True,
-    )
-    # 버튼
-    col1, col2 = st.columns([1, 1])  # 비율 col1:col2
-    with col1:
-        st.text('')  # 줄바꿈
-        st.button(label="caption", on_click=caption('clicked'))
+    # 화면
+    col1, col2, col3 = st.columns([0.2, 1, 0.2])  # 비율 col1:col2:col3
     with col2:
+        st.markdown(
+            f'<img src="data:image/gif;base64,{data_url}" alt="job-interview" width="100%">',
+            unsafe_allow_html=True,
+        )
+        col_l, col_m, col_r = st.columns([0.1, 1, 0.1])  # 비율 col_l:col_m:col_r
+        with col_m:
+            # Recording
+            record()
+    co_button, co_caption = st.columns([0.4, 1])  # 비율 co_button:co_caption
+    with co_button:
+        st.text('')  # 줄바꿈
+        # 버튼
+        st.button(label="caption", on_click=caption(question))
+    with co_caption:
         st.text('')  # 줄바꿈
         st.text(st.session_state['caption'])
-    # Recording
-    record()
 
 
 if __name__ == '__main__':
